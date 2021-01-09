b289b743e5446f03df3ea5c9f5357328cba85f94
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2021-01-09 15:31:42 +0000

    Made the sidebar and sidebarChannel components

---
+++ src/Sidebar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Hardeep Gurwara</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar__addChannel" />
+        </div>
+        <div className="sidebar__channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
