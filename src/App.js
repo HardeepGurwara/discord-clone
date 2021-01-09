ded08a860dfeb2bac4a4112a38540cf569cae99f
PGrover2001 <40870043+PGrover2001@users.noreply.github.com>
2021-01-09 20:10:59 +0000

    added the login functionality, made the login peristent and have made connected the login functionality with state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Chat from "./Chat";
-import { selectUser } from "./features/userSlice";
+import { login, selectUser, logout } from "./features/userSlice";
+import { auth } from "./firebase";
 import Login from "./Login";
 import Sidebar from "./Sidebar";
 
 function App() {
+  const dispatch = useDispatch();
   const user = useSelector(selectUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log("user,", authUser);
+      if (authUser) {
+        dispatch(
+          login({
+            uid: authUser.uid,
+            photo: authUser.photoURL,
+            email: authUser.email,
+            displayName: authUser.displayName,
+          })
+        );
+      } else {
+      }
+    });
+  }, []);
   return (
     <div className="app">
       {user ? (
--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,11 @@
 import { Button } from "@material-ui/core";
 import React from "react";
 import "./Login.css";
+import { auth, provider } from "./firebase";
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth.signInWithPopup(provider).catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <div className="login__logo">
